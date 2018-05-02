b70f66e6611d68bdfeb78fc8a6f848aaec260420
Мгер <fryn3@mail.ru>
2018-05-02 19:33:07 +0300

    заготовка для объядинения вершин и стрелок

--- scene.cpp
+++ scene.cpp
@@ -1,6 +1,16 @@
 #include "scene.h"
 
-Scene::Scene()
+Scene::Scene(QObject *parent): QGraphicsScene(parent)
 {
 
 }
+
+void Scene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    if (event->buttons() == Qt::MouseButton::LeftButton && selectedItems().size() == 1) {
+        // тут должны перерисовать стрелки!
+    }
+    QGraphicsScene::mouseMoveEvent(event);
+}
+
+
--- scene.h
+++ scene.h
@@ -1,11 +1,25 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QGraphicsView>
+#include <QPushButton>
+#include <QVector>
+#include <QMap>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMap>
+#include "node.h"
+#include "arrowitemgroup.h"
 
 class Scene : public QGraphicsScene
 {
 public:
-    Scene();
+    Scene(QObject *parent = Q_NULLPTR);
+protected:
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
 };
 
 #endif // SCENE_H
